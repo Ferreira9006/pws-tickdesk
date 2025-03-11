e5fc842990bfe1e61ba67d5a8ad0ce5c2d096d3a
Gabriel Ferreira <ferreirawow@gmail.com>
2025-03-11 22:13:46 +0000

    add fillable fields to models

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
-    //
+    protected $fillable = [
+        'title', 
+        'description', 
+        'category_id', 
+        'priority_id', 
+        'level_id', 
+        'user_id', 
+        'status', 
+        'closed_at'
+    ];
 }
