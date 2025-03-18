6284408c366a30179c0741aa9334dd435144b7dd
Gabriel Ferreira <ferreirawow@gmail.com>
2025-03-18 21:44:14 +0000

    add admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+// Needed for the admin route format. If using the other method, it's not needed.
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+
+/* 
+Route::get('/admin', 'AdminController@index')->name('admin.index');
+This is the other method of defining routes. 
+If using this method, the use statement for HomeController is not needed.
+*/   
